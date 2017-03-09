01eec1070fb25b82842ffc4f0b8fe7032247a17e
Roman Ostapchuk <rostapch@e1r10p1.unit.ua>
2017-03-09 18:41:58 +0200

    starting Fdf

---
+++ Fdf/main.c
@@ -0,0 +1,23 @@
+#include "mlx.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int	KeyPress(int key)
+{
+	if (key == 53)
+		exit(0);
+	return (0);
+}
+
+int		main(void)
+{
+	void *mlx;
+	void *wnd;
+
+	mlx = mlx_init();
+	wnd = mlx_new_window(mlx, 400, 400, "mlx 42");
+	mlx_pixel_put(mlx, wnd, field_x[i][j], field_y[i][j], 0x00FFFFFF);
+
+	mlx_key_hook(wnd, KeyPress, mlx);
+	mlx_loop(mlx);
+}
