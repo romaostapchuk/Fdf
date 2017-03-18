56ac218c2b0800da5851580956723f55495b95b2
Roman Ostapchuk <rostapch@e1r10p1.unit.ua>
2017-03-18 19:32:14 +0200

    no random dots while drawing

--- drawing.c
+++ drawing.c
@@ -12,34 +12,40 @@
 
 #include "fdf.h"
 
+int		min_of_2(double a, double b)
+{
+	if (a <= b)
+		return (0);
+	return (1);
+}
+
 void	draw_line(double *xy1, double *xy2, void *mlx, void *wnd)
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 
-	if (fabs(xy2[0] - xy1[0]) > fabs(xy2[1] - xy1[1]))
+	y = fmin(xy1[1], xy2[1]);
+	x = fmin(xy1[0], xy2[0]);
+	if (fabs(xy2[0] - xy1[0]) >= fabs(xy2[1] - xy1[1]))
 	{
-		y = fmin(xy1[1], xy2[1]);
-		x = fmin(xy1[0] - 1, xy2[0] - 1);
-		while (++x <= fmax(xy2[0], xy1[0]))
+		y = min_of_2(xy1[0], xy2[0]) == 0 ? xy1[1] : xy2[1];
+		while (x <= fmax(xy2[0], xy1[0]))
 		{
 			mlx_pixel_put(mlx, wnd, x, y, 0x00FFFFFF);
 			y = (xy2[1] - xy1[1]) / (xy2[0] - xy1[0]) * (x - xy1[0]) + xy1[1];
-			//printf("<%d><%d>  _", x, y);
+			x++;
 		}
 	}
 	else
 	{
-		y = fmin(xy1[1] - 1, xy2[1] - 1);
-		x = fmin(xy1[0], xy2[0]);
-		while (++y <= fmax(xy2[1], xy1[1]))
+		x = min_of_2(xy1[1], xy2[1]) == 0 ? xy1[0] : xy2[0];
+		while (y <= fmax(xy2[1], xy1[1]))
 		{
 			mlx_pixel_put(mlx, wnd, x, y, 0x00FFFFFF);
 			x = (xy2[0] - xy1[0]) / (xy2[1] - xy1[1]) * (y - xy1[1]) + xy1[0];
-			//printf("<%d><%d>  _", x, y);
+			y++;
 		}
 	}
-			//printf("\n");
 }
 
 void	draw_field(double ***tab, int x, int y)
@@ -60,11 +66,6 @@ void	draw_field(double ***tab, int x, int y)
 				draw_line(tab[i][j], tab[i + 1][j], mlx, wnd);
 			if (j < x - 1)
 				draw_line(tab[i][j], tab[i][j + 1], mlx, wnd);
-			else if (i == y - 1 && j == x - 1)
-			{
-				draw_line(tab[i][j], tab[i][j - 1], mlx, wnd);
-				draw_line(tab[i][j], tab[i - 1][j], mlx, wnd);
-			}
 		}
 	}
 }
